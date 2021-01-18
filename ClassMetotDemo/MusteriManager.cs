8c700e5715fdacdb7b0a305ee745f4e357579d37
Büşra Kızılgül <1030516356@erciyes.edu.tr>
2021-01-18 22:57:27 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        //müşteri ekle, listele, sil işlemleri
+        public void Ekle (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi" + musteri.Id);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi." + musteri.Id);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listelendi" + musteri.Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "Güneş";
+            musteri1.Numara = 12345;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Ayşe";
+            musteri2.Soyadi = "Yıldız";
+            musteri2.Numara = 23456;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Cemal";
+            musteri3.Soyadi = "Gündüz";
+            musteri3.Numara = 34567;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+        }
+
+    }
+}
